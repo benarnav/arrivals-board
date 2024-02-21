44d6e02ff9b3127d809c54afeeb5a56a4408ec7e
b arnav <117926567+benarnav@users.noreply.github.com>
2024-02-21 11:47:56 -0500

    fix typo

--- envar_template.py
+++ envar_template.py
@@ -1,5 +1,5 @@
-envar ={'api_key':"", #api key for users trying to access endpoints from the Flask app
+envar ={'api_key':"", #api key for users trying to access api endpoints in the Flask app
         "mta_key": "", #api key for MTA real time feeds, from https://api.mta.info
-        "wmata_key":"", #api ket for WMATA feeds, from https://developer.wmata.com
+        "wmata_key":"", #api key for WMATA feeds, from https://developer.wmata.com
     
 }
