7d5989c233a744e1cc8c46638cb1061c3377e945
b arnav <117926567+benarnav@users.noreply.github.com>
2024-06-11 22:30:17 -0400

    remove MTA api key, no longer needed

--- envar_template.py
+++ envar_template.py
@@ -1,5 +1,4 @@
-envar ={'api_key':"", #api key for users trying to access api endpoints in the Flask app
-        "mta_key": "", #api key for MTA real time feeds, from https://api.mta.info
-        "wmata_key":"", #api key for WMATA feeds, from https://developer.wmata.com
-    
+envar = {
+    "api_key": "",  # api key for users trying to access api endpoints in the Flask app
+    "wmata_key": "",  # api key for WMATA feeds, from https://developer.wmata.com
 }
