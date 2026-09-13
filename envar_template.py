a1c10e5f49a42a4b8245958d717d05e125395279
benjamin <117926567+benarnav@users.noreply.github.com>
2026-09-12 18:09:42 -0700

    Remove deprecated files: `envar_template.py`, `station_dict.py`, and `transit_api.py` to streamline the codebase and eliminate unused components.

--- envar_template.py
+++
@@ -1,4 +0,0 @@
-envar = {
-    "api_key": "",  # api key for users trying to access api endpoints in the Flask app
-    "wmata_key": "",  # api key for WMATA feeds, from https://developer.wmata.com
-}
